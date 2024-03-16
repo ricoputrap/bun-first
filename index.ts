7ea24e63a08f3dd67753342801c81f95bc428100
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-03-16 08:15:36 +0700

    Add a simple routing and proper error handling

--- index.ts
+++ index.ts
@@ -1,7 +1,30 @@
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("HELLO WORLD YEAHHH!");
+    const url = new URL(req.url);
+
+    switch (url.pathname) {
+      case "/":
+        return new Response("HOME");
+
+      case "/about":
+        return new Response("About me!");
+
+      case "/contact":
+        return new Response("Contact us!");
+
+      default:
+        throw new Error("Could not find " + url.pathname);
+    }
+  },
+
+  error(error) {
+    return new Response(`<pre> ${error} \n ${error.stack}</pre>`, {
+      headers: {
+        'Content-Type': 'text/html',
+      }
+    }
+    )
   }
 });
 
